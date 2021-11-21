b4c91a2f79cac7cc48171fc3d83492ac1c3a8f69
Can Sayan <d.cansayan@hotmail.com>
2021-11-21 07:41:34 +0300

    Order service domain

---
+++ Ordering.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ordering.Domain.Entities.Base
+{
+    public abstract class Entity : IEntityBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; protected set; }
+
+        public Entity Clone()
+        {
+            return (Entity)this.MemberwiseClone();
+        }
+    }
+}
